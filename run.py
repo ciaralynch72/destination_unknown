abdb311522f8a02dc5aae5376be26d05448c9651
ciaralynch72 <Ciaralynch72@gmail.com>
2022-08-11 19:36:38 +0000

    Import libraries, create and import words.py file

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Used to choose a random country for the game
+import random
+
+# Used to exit the game when the player chooses
+# not to continue at the end
+import sys
+
+# Used to import a list of countries around the world
+from words import country
+
+# Used to import color for the text throughout the game
+from colorama import Fore, Back, Style
+# Resets the color everytime
+colorama.init(autoreset=True)
+
+# Used to create the opening and closing graphic
+import pyfiglet
+
