e04b2da3af77f3f6bf3cc6cdf60b013762061bc9
ciaralynch72 <Ciaralynch72@gmail.com>
2022-08-12 15:19:38 +0000

    Fix errors for PEP8 validation

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ import random
 
 import sys  # used to exit the game
 
+import os  # used to clear the screen
+
 # Used to import a list of countries around the world
 from words import country
 
@@ -12,7 +14,7 @@ import time  # Used to control the pace of the display
 
 import colorama  # Used to import color for the text throughout the game
 from colorama import Fore, Back, Style
-colorama.init(autoreset=True)  #  Resets the color everytime
+colorama.init(autoreset=True)  # Resets the color everytime
 
 import pyfiglet  # Used to create the opening and closing graphic
 
@@ -32,27 +34,36 @@ def introduction_message():
             print(f"{Fore.LIGHTYELLOW_EX}Your name must be alphabetic only")
             continue
         else:
-            print(Fore.RESET + "Destination unknown is a word guessing game")
+            print(Fore.RESET + "Destination unknown is a word guessing game.")
             print("where you are trying to guess the name of a country!")
-            print("You chances will be based on 1.5 * the lenght of letters in the country")
+            print("Guess one letter at a time")
+            print("Your chances will be based on 1.5 * the lenght of letters")
+            print("in the countries name.")
+            print("If you guess correct your chances remain the same.")
+            print("If you guess incorrect then you will lose a chance")
+            print("Correct letters will show up in the right order")
+            print("to help you figure out the destination.")
             print(f'Good luck, {name}!')
             break
 
+
 def choose_word():
     """
-    Selects a random country from the words.py file and 
+    Selects a random country from the words.py file and
     deselects any countries which name contails a space
     or a hyphen. Also returns a lower case word.
     """
     word = random.choice(country)
+
     while '-' in word or ' ' in word:
-        word= random.choice(country)
+        word = random.choice(country)
     return word.lower()
+
 
 def current_country(word, guesses):
     """
     If the players guess is corret the letter will show
-    in the word. if not an asterik will show until the 
+    in the word. if not an asterik will show until the
     user guesses the correct letter eg. *a*a*a
     """
     current_country = ""
@@ -63,10 +74,11 @@ def current_country(word, guesses):
             current_country += "*"
     return current_country
 
-# Variable to create graphic if the player guesses the country. 
+
+# Variable to create graphic if the player guesses the country.
 FIREWORKS = """
                                  .''.
-       .''.             *''*    :_\/_:     . 
+      .''.             *''*    :_\/_:     . 
       :_\/_:   .    .:.*_\/_*   : /\ :  .'.:.'.
   .''.: /\ : _\(/_  ':'* /\ *  : '..'.  -=:o:=-
  :_\/_:'.:::. /)\*''*  .|.* '.\'/.'_\(/_'.':'.'
@@ -80,31 +92,33 @@ FIREWORKS = """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
+
 def start_game(word):
     """
     Function used to start and run the game. The includes the code
-    for the player to take a guess, the amount of chances they have 
+    for the player to take a guess, the amount of chances they have
     left and lets the user know if they have won or not.
     """
-    # create an emply list to display player guesses 
+    # create an emply list to display player guesses
     player_guesses = []
     # Gives the player 1.5 times the lenght of the word to guess the country
     chances = len(word)*int(1.5)
-    print(Fore.CYAN + "You are looking for a country that is " + str(len(word)) + " letters long.")
+    print(Fore.CYAN + "The country is " + str(len(word)) + " letters long.")
     while True:
         if chances != 0:
             print("\nYou have " + str(chances) + " chances left.")
             time.sleep(1)
             print("Country so far: " + current_country(word, player_guesses))
             time.sleep(1)
-            print(Fore.LIGHTGREEN_EX + "Letters guessed: " + str(player_guesses))
+            print(Fore.CYAN + "Letters guessed: " + str(player_guesses))
             guess = input("Guess: ").lower()[0]
 
             if guess not in player_guesses:
                 player_guesses.append(guess)
+                clear_screen()
 
             if current_country(word, player_guesses) == word:
-                print("\nCongratulations! You guessed the right country: " + word)
+                print(Fore.GREEN + "Congrats" + word.upper() + "was correct")
                 print(FIREWORKS)
                 break
 
@@ -112,35 +126,45 @@ def start_game(word):
                 if guess in word:
                     print(Fore.GREEN + "Correct letter!")
                 else:
-                    print(guess + " is not in the word.")
+                    print(Fore.YELLOW + guess + " is not in the destination.")
                     chances -= 1
         else:
-            print(Fore.RED + "Hard luck! The correct country was " + word.upper())
+            print(Fore.RED + "Hard luck! The destination was " + word.upper())
             break
+
 
 def goodbye_message():
     """
     Final function which ends the game if the
-    player wishes. It also prints Goodbye message 
+    player wishes. It also prints Goodbye message
     with pyfiglet.
     """
     print(pyfiglet.figlet_format(f"Goodbye!"))
     sys.exit()
 
+
 def play_again():
     """
-    This function gives the user teh option to play 
-    again or finish the game. 
+    This function gives the user teh option to play
+    again or finish the game.
     """
     play_again = input("Would you like to continue yes or no?: ")
     if play_again == ("yes" or "YES"):
         choose_word()
     elif play_again == ("no" or "NO"):
-        print("Thank you for playing")
+        print("Thanks you for playing")
         goodbye_message()
     else:
         print("Sorry invalid entry.")
-        print("Plese enter yes or no.") 
+        print("Plese enter yes or no.")
+
+
+def clear_screen():
+    """
+    Used to clear Terminal screen
+    Credit: https://www.101computing.net/python-typing-text-effect/
+    """
+    os.system("clear")
 
 while True:
     """
