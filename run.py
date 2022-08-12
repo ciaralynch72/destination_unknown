c555fdd59dd9a028a8bd55e7707bb739b6721deb
ciaralynch72 <Ciaralynch72@gmail.com>
2022-08-12 00:21:04 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -30,3 +30,39 @@ def choose_word():
         word= random.choice(country)
     return word.lower()
 
+# Function to show correct letter guessed in the correct position in the current country
+def current_country(word, guesses):
+    current_country = ""
+    for letter in word:
+        if letter in guesses:
+            current_country =+ letter
+        else:
+            current_country =+ "*"
+    return current_country
+
+# Global variable used to create a fun display if the user guesses the correct country
+FIREWORKS = """
+                                 .''.
+       .''.             *''*    :_\/_:     . 
+      :_\/_:   .    .:.*_\/_*   : /\ :  .'.:.'.
+  .''.: /\ : _\(/_  ':'* /\ *  : '..'.  -=:o:=-
+ :_\/_:'.:::. /)\*''*  .|.* '.\'/.'_\(/_'.':'.'
+ : /\ : :::::  '*_\/_* | |  -= o =- /)\    '  *
+  '..'  ':::'   * /\ * |'|  .'/.\'.  '._____
+      *        __*..* |  |     :      |.   |' .---"|
+       _*   .-'   '-. |  |     .--'|  ||   | _|    |
+    .-'|  _.|  |    ||   '-__  |   |  |    ||      |
+    |' | |.    |    ||       | |   |  |    ||      |
+ ___|  '-'     '    ""       '-'   '-.'    '`      |____
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+# Main funtion to run the word game. This includes the code for the player too
+# input their name, take a guess, the amount of chances they have left and
+# lets the user know if they have won or not
+
+def start_game(word):
+    # create an emply list to display player guesses 
+    player_guesses = []
+    # Gives the player 1.5 times the lenght of the word to guess the country
+    chances = len(word)*int(1.5)
